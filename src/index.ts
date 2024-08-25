746e0dde226a2dc9996e8d912fe73fb51839f7ca
Aayush <ayushkarkee6@gmail.com>
2024-08-25 16:14:11 +0545

    creating user model

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import express, {Request, Response} from 'express';
 import cors from 'cors';
 import "dotenv/config";
-import mongoose from 'mongoose';
-
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+import { dbConnection } from './database/dbConnection';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
+
+dbConnection();
 
 app.get("/api/test", async (req: Request, res: Response) => {
     res.json({message: "Nigga"})
