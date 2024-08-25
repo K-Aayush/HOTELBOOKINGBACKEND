0ba3bc025949d224eb33872d630e7e5850215f54
Aayush <ayushkarkee6@gmail.com>
2024-08-25 16:22:01 +0545

    register post req without cookie

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,11 +1,21 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
+import User from "../models/user";
 
 const router = express.Router();
 
 router.post("/register", async (req: Request, res: Response) => {
-    try {
-        
-    } catch (error) {
-        
+  try {
+    let user = await User.findOne({
+      email: req.body.email,
+    });
+
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
     }
-})
+
+    user = new User(req.body);
+    await user.save();
+  } catch (error) {
+    res.status(500).send({ message: "Something went wrong" });
+  }
+});
