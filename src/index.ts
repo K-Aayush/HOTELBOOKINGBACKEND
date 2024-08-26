a57795300fe348d6c343f9590f37697ef183d43b
Aayush <ayushkarkee6@gmail.com>
2024-08-26 14:59:47 +0545

    register with jwt cookie

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
-import express, {Request, Response} from 'express';
-import cors from 'cors';
+import express, { Request, Response } from "express";
+import cors from "cors";
 import "dotenv/config";
-import { dbConnection } from './database/dbConnection';
+import { dbConnection } from "./database/dbConnection";
+import userRoutes from "./routes/users";
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 dbConnection();
 
-app.get("/api/test", async (req: Request, res: Response) => {
-    res.json({message: "Nigga"})
-})
+app.use("/api/users", userRoutes);
 
 app.listen(3000, () => {
-    console.log("Server running on port 3000")
+  console.log("Server running on port 3000");
 });
