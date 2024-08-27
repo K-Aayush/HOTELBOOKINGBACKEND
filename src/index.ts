ef941de018fd5f4a6eed2967ea839f154af802e4
Aayush <ayushkarkee6@gmail.com>
2024-08-27 14:42:14 +0545

    Login api

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import { dbConnection } from "./database/dbConnection";
 import userRoutes from "./routes/users";
+import authRoutes from "./routes/auth";
 
 const app = express();
 app.use(express.json());
@@ -12,6 +13,7 @@ app.use(cors());
 dbConnection();
 
 app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
