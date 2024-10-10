a67edcb8d65f06bc29513371d9c7ccc1059b3e61
Aayush <ayushkarkee6@gmail.com>
2024-10-10 13:10:28 +0545

    cloudinary configuration

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,13 @@ import { dbConnection } from "./database/dbConnection";
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 app.use(cookieParser());
